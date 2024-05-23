a18ce68e45e99edfb52ed18668aa28f8cc492a33
GoutamK1902 <97177447+GoutamK1902@users.noreply.github.com>
2024-05-23 21:35:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.getElementById("text-input");
+  const checkButton = document.getElementById("check-btn");
+  const resultText = document.getElementById("result");
+
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      palindromChecker();
+    }
+  });
+
+  checkButton.addEventListener("click", palindromChecker);
+
+  function palindromChecker() {
+    let inputValue = input.value;
+    // Remove non-alphanumeric characters and convert to lowercase
+    let str = inputValue.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let reverseStr = str.split("").reverse().join("");
+    resultText.classList.remove("hide");
+    setTimeout(() => {
+      resultText.classList.add("hide");
+    }, 5000);
+    if (inputValue.trim() === "") {
+      resultText.classList.add("hide");
+      alert("Please input a value");
+    } else if (str === reverseStr) {
+      resultText.innerText = `${inputValue} is a palindrome`;
+    } else {
+      resultText.innerText = `${inputValue} is not a palindrome`;
+    }
+  }
+});
